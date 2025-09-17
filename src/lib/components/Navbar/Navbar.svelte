44f25f4e017ed4d3e1dedc066dca5c353b3b8225
ayyaya21 <66070221@kmitl.ac.th>
2025-09-17 13:14:36 +0700

    Update: Init Project

---
+++ src/lib/components/Navbar/Navbar.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+    import * as DropdownMenu from "$lib/components/ui/dropdown-menu/index.js";
+</script>
+
+<nav class="flex justify-between m-2">
+    <h4 class="scroll-m-20 text-xl font-semibold tracking-tight">
+        Check-in-easy
+    </h4>
+    <DropdownMenu.Root>
+        <DropdownMenu.Trigger>Profiles</DropdownMenu.Trigger>
+        <DropdownMenu.Content>
+          <DropdownMenu.Group>
+            <DropdownMenu.Label>My Account</DropdownMenu.Label>
+            <DropdownMenu.Separator />
+            <DropdownMenu.Item>Profile</DropdownMenu.Item>
+            <DropdownMenu.Item>Logout</DropdownMenu.Item>
+          </DropdownMenu.Group>
+        </DropdownMenu.Content>
+      </DropdownMenu.Root>
+      
+</nav>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import Navbar from "$lib/components/Navbar/Navbar.svelte";
+	import { page } from "$app/stores";
+
+	let { children } = $props();
+	import "../app.css";
+</script>
+
+{#if $page.url.pathname !== "/"}
+	<Navbar />
+{/if}
+
+{@render children?.()}
