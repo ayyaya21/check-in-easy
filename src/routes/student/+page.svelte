e7617ecac3b5d2d3d8f1e51ec4a08e83b457f0a2
Firsteria <141379353+Firsteria@users.noreply.github.com>
2025-10-07 01:06:12 +0700

    myfirstcommit
    tester

--- src/routes/student/+page.svelte
+++ src/routes/student/+page.svelte
@@ -1,2 +1,16 @@
-<script></script>
-<div>THIS IS STUDENT</div>
+<script lang="ts">
+ import { getLocalTimeZone, today } from "@internationalized/date";
+ import { Calendar } from "$lib/components/ui/calendar/index.js";
+ 
+ let value = today(getLocalTimeZone());
+</script>
+ 
+<div>
+
+    <Calendar
+    type="single"
+    bind:value
+    class="rounded-md border shadow-sm"
+    captionLayout="dropdown"
+    />
+</div>
