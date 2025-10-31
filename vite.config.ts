cba0833c1bfd385f0e628cfbc0d86b4264506627
ayyaya21 <66070221@kmitl.ac.th>
2025-10-31 17:22:06 +0700

    update: check origin

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,6 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
 	plugins: [sveltekit(), tailwindcss()],
 	server: {
-		allowedHosts: ['a3f67d70abaf.ngrok-free.app']
-	}
+		allowedHosts: true,
+	},
 });
