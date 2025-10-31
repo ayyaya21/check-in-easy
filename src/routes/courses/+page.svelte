ac43a5e4e12187bb44ff2b0f5599b93dd4eac636
Zensos <rockemaner@gmail.com>
2025-10-31 22:49:01 +0700

    ui: change card color

--- src/routes/courses/+page.svelte
+++ src/routes/courses/+page.svelte
@@ -5,7 +5,7 @@
 
 <div class="max-w-5xl mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 justify-center gap-6 p-10 place-items-center w-fit">
   {#each courses as c}
-    <a href={`/courses/${c.id}`} class="bg-blue-500 bg-[url('https://www.transparenttextures.com/patterns/squared-metal.png')] w-75 rounded-2xl shadow-md overflow-hidden hover:-translate-y-1 hover:shadow-lg transition-all duration-300">
+    <a href={`/courses/${c.id}`} class="bg-orange-500 bg-[url('https://www.transparenttextures.com/patterns/squared-metal.png')] w-75 rounded-2xl shadow-md overflow-hidden hover:-translate-y-1 hover:shadow-lg transition-all duration-300">
       <div class={`h-40 bg-cover bg-center`}></div>
       <div class="p-4 bg-white">
         <div class="text-blue-500 font-semibold">{c.code}</div>
